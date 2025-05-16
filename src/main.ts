5a8bc8c5cc7535627a9dfbae3e7e4d8f74a30ca2
ShuShuicu <Tomori@miomoe.cn>
2025-05-16 16:23:10 +0800

    plugins

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { registerPlugins } from '@/plugins'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+const app = createApp(App);
+registerPlugins(app); // 注册插件
+app.mount('#app');
